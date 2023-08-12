7569c75a8d7c9910ac05096574b5294174c13b02
Keon24 <keonpowell95@gmail.com>
2023-08-12 01:15:26 -0700

    Manipulate Dom

---
+++ script.js
@@ -0,0 +1,20 @@
+//Use Dom manipulation to update the html elements
+const locationNameElement = document.querySelector('location-name');
+const temperatureElement = document.querySelector('temperature');
+const weatherConditionsElement = document.querySelector('weather-conditions');
+const getTemperature = document.querySelector('.forecast-temp');
+const forecastConditionsElement = document.querySelector('.forecast-conditions');
+
+
+
+//Add event listeners
+//Api key with users location
+//Make a get request
+//Handle the api request
+//Extract relevant weather data from api repsomse 
+//Convert temperature umits amd format the data
+//Display the temperature weather conditions icons
+//Implement error hamdlimg where the API request fails
+//Update the UI tp display error messages
+//Implement Users geolocation 
+//Use the geolocation API to to obtain coordinats and make an API request 
