c3f65278b357d8580a2fad734ed5ef0ddc197990
Keon24 <keonpowell95@gmail.com>
2023-08-14 10:33:28 -0700

    Updated The Dom

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //Use Dom manipulation to update the html elements
-const locationNameElement = document.querySelector('location-name');
-const temperatureElement = document.querySelector('temperature');
-const weatherConditionsElement = document.querySelector('weather-conditions');
-const getTemperature = document.querySelector('.forecast-temp');
-const forecastConditionsElement = document.querySelector('.forecast-conditions');
+const locationNameElement = document.getElementById('#location-name');
+const temperatureElement = document.getElementById('#temperature');
+const weatherConditionsElement = document.getElementById('#weather-conditions');
+const forecastTempElements = document.querySelectorAll('.forecast-temp');
+const forecastConditionsElement = document.querySelectorAll('.forecast-conditions');
+const errorElement = document.getElementById('error-message');
+const fetchButton = document.getElementById('search-button');
+const unitToggle = document.getElementById('unit-toggle');
+
 
 const weatherData = {
 locatiion: 'New York',
@@ -25,13 +29,17 @@ temperatureElement.textContent = weatherData.temperature;
 weatherConditionsElement.textContent = weatherData.conditions;
 forecastTempElement.textContent = weatherData.forecast[0].temp;
 forecastTempElement.textContent = weatherData.forecast[0].forecastConditions;
-
-updateWeatherData(intialWeatherData);
-
+errorElement.textContent = '';
 }
-//Add event listeners
-//Api key with users location
+
+
+
+ //Fetch weather data from API
+
+
 //Make a get request
+
+//Add event listeners
 //Handle the api request
 //Extract relevant weather data from api repsomse 
 //Convert temperature umits amd format the data
